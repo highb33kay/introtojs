897ec2353c9a58ad809499f5162773fcc39b6961
highb33kay <hiibeekayvibe@gmail.com>
2022-06-23 10:07:13 +0100

    made a lot of changes

---
+++ me.js
@@ -0,0 +1,10 @@
+let names = "Ibukun Alesinloye"
+let height = 154;
+let country = "Nigeria";
+
+alerting = `Hello, My name is ${names}. I am from ${country} and I am ${height} cm tall.`;
+console.log(alerting)
+// print alerting
+document.write(alerting);
+// alert alerting
+alert(alerting)
